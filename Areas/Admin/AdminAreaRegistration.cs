24bdc73e20b8e15f97972572d8977b663a996003
João Canoso <jpcanoso@hotmail.com>
2018-10-09 03:00:04 +0100

    Added Admin Area

---
+++ Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetSimpleAdmin.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                //new { action = "Index", id = UrlParameter.Optional }
+                // Initial Controller
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
